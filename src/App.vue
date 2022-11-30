ea08bb2bcd14c8e6d82e4f6265b0d8ce821c5a78
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="p-4 m-4">
+    <h1 class="text-xl font-medium text-gray-500">
+      CrewPass Agency Integration Testing Popup
+    </h1>
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-xl">Hello</div>
+</template>
